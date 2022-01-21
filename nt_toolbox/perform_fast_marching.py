32657ea1549cf8ffe318980e9b5947250f4e4a6c
Théo BERTRAND <tbertrand@ceremade.dauphine.fr>
2022-01-21 18:49:22 +0100

    Make perform_fast_marching right

--- nt_toolbox/perform_fast_marching.py
+++ nt_toolbox/perform_fast_marching.py
@@ -1,12 +1,13 @@
 import numpy as np
 import skfmm as fmm
 
-def perform_fast_marching(D,start_points):
+def perform_fast_marching(W, start_points, periodic=False):
     """
         Implementation of the fast marching algorithm in 2Ds using the skfmm library
-        D : 2D weight matrix, must be positive
+        W : 2D weight matrix, must be positive
         start_points : 2 x n array, start_points[:,i] is the ith starting point
     """
-    D_temp = np.copy(D)
+    D_temp = np.ones_like(W) # 0-levelsets indicate boundary from which we compute distance
     D_temp[start_points[0,:],start_points[1,:]] = 0
-    return fmm.distance(D_temp) + 1e-15
+    return fmm.distance(D_temp, speed=W, periodic=periodic) + 1e-15
+    
